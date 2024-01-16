f99959555deaa7151ed3b477741b58e8ec5e9cd4
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-16 11:50:10 +0530

    refactor: remove unnecessary codes

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
