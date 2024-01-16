ff1c96564870169261b694b4fb946231b23d9130
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-16 13:14:23 +0530

    feat: update IconComponent to return x, o or ? icon

--- src/component/IconComponent/index.tsx
+++ src/component/IconComponent/index.tsx
@@ -3,14 +3,23 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 interface IconComponentProps {
   name: string;
-  color: string;
   size?: number;
 }
 
-export function IconComponent({
-  name,
-  color,
-  size,
-}: IconComponentProps): JSX.Element {
-  return <Icon name={name} color={color} size={size ?? 24} />;
+export function IconComponent({ name, size }: IconComponentProps): JSX.Element {
+  let component = (
+    <Icon name="question-circle" color="#DAE0E2" size={size ?? 24} />
+  );
+
+  switch (name) {
+    case "x":
+      component = <Icon name="times" color="#45CE30" size={size ?? 24} />;
+      break;
+    case "o":
+      component = <Icon name="circle-thin" color="#F0DF87" size={size ?? 24} />;
+      break;
+    default:
+      break;
+  }
+  return component;
 }
