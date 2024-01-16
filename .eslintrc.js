3d68f649ba6263437aa94ff5947e8eb5505e7544
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-16 11:55:50 +0530

    build: streamline linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,46 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  plugins: ["import"],
+  extends: "@react-native",
+  overrides: [
+    {
+      files: ["*.js", "*.ts", "*.jsx", "*.tsx"],
+      rules: {
+        quotes: ["warn", "double"],
+      },
+    },
+    {
+      files: ["*.js", "*.ts", "*.jsx", "*.tsx"],
+      rules: {
+        "import/order": [
+          "error",
+          {
+            groups: [
+              ["builtin", "external"],
+              "internal",
+              ["parent", "sibling", "index"],
+              ["type"],
+            ],
+            pathGroups: [
+              {
+                pattern: "{react,react-dom,react-native}",
+                group: "external",
+                position: "before",
+              },
+            ],
+            pathGroupsExcludedImportTypes: [
+              "react",
+              "react-dom",
+              "react-native",
+            ],
+            alphabetize: {
+              order: "asc",
+              caseInsensitive: true,
+            },
+          },
+        ],
+        "@typescript-eslint/consistent-type-imports": "error",
+      },
+    },
+  ],
 };
